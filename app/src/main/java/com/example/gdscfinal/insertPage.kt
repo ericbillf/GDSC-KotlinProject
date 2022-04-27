21e0c963f47912cd19b6c345df0b70d97d5eba0e
TaYu <erici7013787@gmail.com>
2022-04-27 15:36:38 +0800

    new insert page

---
+++ app/src/main/java/com/example/gdscfinal/insertPage.kt
@@ -0,0 +1,11 @@
+package com.example.gdscfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class insertPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_page)
+    }
+}
