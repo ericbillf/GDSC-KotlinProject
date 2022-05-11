1819e9fcfc3a7cf34e30c3e65993972d6ee4268d
Chen <eva515153@gmail.com>
2022-05-11 23:15:43 +0800

    還是沒辦法把新聞標題放到主頁QQ，所以今天做了布局和把新聞圖片的網址抓下來，但是不知道要怎麼把網址變圖片。裡面註解有說哪裡報錯

--- app/src/main/java/com/example/gdscfinal/MainActivity.kt
+++ app/src/main/java/com/example/gdscfinal/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.gdscfinal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import org.jsoup.Jsoup
 
 
@@ -14,29 +12,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val thread = Thread{
 
+        val thread = Thread(Runnable{
             val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
-            val span=doc.select("h2 > a")
-
+            val span = doc.select("h2 > a")
+            val img = doc.select("picture > img")
             Log.i("----------", "---------")
-            for(x in span) {
+            for (x in span) {
                 Log.i("result", x.attr("title").toString())
             }
-            //正在研究怎麼把抓下來的新聞標題放到頁面
-            var array= arrayOf<String>()
-            for(x in span) {
-                for (y in 1..10) {
-                    array[y] = x.attr("title").toString()
-                }
+            for(x in img){
+                Log.i("img",x.attr("data-src").toString())
             }
+        }).start()
 
-            val list=findViewById<ListView>(R.id.listview)
-            val arrayAdapter= ArrayAdapter(this, android.R.layout.simple_list_item_1,array)
-            list.adapter=arrayAdapter
+
+//報錯都有這行,不裝在thread裡就會報錯,打不開app val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
+            val span = doc.select("h2 > a")
+            val list = findViewById<ListView>(R.id.listview)
+            val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+            for (x in span) {
+                arrayAdapter.add(x.attr("title").toString())
+            }
+            list.adapter = arrayAdapter
+
+
+//不用arrayAdapter的方法,但還是在43行報錯
+        val textview = findViewById<TextView>(R.id.textview)
+        val imgview = findViewById<ImageView>(R.id.imageView)
+        val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
+        val span = doc.select("h2 > a")
+        val img = doc.select("picture > img")
+        for (x in span) {
+            textview.setText(x.attr("title").toString())
+
         }
-        thread.start()
     }
 }
-
 
