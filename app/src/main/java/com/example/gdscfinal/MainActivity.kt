c024c4cfd18b5215dbd6e48b66053c5855bdf688
Chen <eva515153@gmail.com>
2022-05-18 22:05:22 +0800

    有把網址抓下來，但是點連結好像有點超出我的能力範圍

--- app/src/main/java/com/example/gdscfinal/MainActivity.kt
+++ app/src/main/java/com/example/gdscfinal/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.gdscfinal
 
+import android.graphics.ImageFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import org.jsoup.Jsoup
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,41 +16,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val thread = Thread(Runnable{
+        Thread(Runnable{
+            val list=findViewById<ListView>(R.id.listview)
             val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
             val span = doc.select("h2 > a")
             val img = doc.select("picture > img")
             Log.i("----------", "---------")
-            for (x in span) {
-                Log.i("result", x.attr("title").toString())
-            }
-            for(x in img){
-                Log.i("img",x.attr("data-src").toString())
-            }
-        }).start()
-
-
-//報錯都有這行,不裝在thread裡就會報錯,打不開app val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
-            val span = doc.select("h2 > a")
-            val list = findViewById<ListView>(R.id.listview)
             val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
             for (x in span) {
-                arrayAdapter.add(x.attr("title").toString())
+                if(x.attr("title").toString()!=""){
+                    Log.i("result",x.attr("title").toString())
+                    arrayAdapter.add(x.attr("title").toString())
+                }
             }
-            list.adapter = arrayAdapter
+            for (x in img) {
+                if(x.attr("data-src").toString()!=""){
+                Log.i("img", x.attr("data-src").toString())
+                }
+            }
+            for (x in span) {
+                if(x.attr("href").toString()!=""){
+                    Log.i("link","https://udn.com/"+x.attr("href").toString())
+                }
+            }
+            runOnUiThread{
+                list.adapter=arrayAdapter
+                /*list.setOnItemClickListener(
 
-
-//不用arrayAdapter的方法,但還是在43行報錯
-        val textview = findViewById<TextView>(R.id.textview)
-        val imgview = findViewById<ImageView>(R.id.imageView)
-        val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
-        val span = doc.select("h2 > a")
-        val img = doc.select("picture > img")
-        for (x in span) {
-            textview.setText(x.attr("title").toString())
+                )*/
+            }
+            /*fun getUrlFromIntent(view: View) {
+                val url = "http://www.google.com"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+            }*/
+        }).start()
 
         }
     }
-}
 
+
+//https://udn.com/
