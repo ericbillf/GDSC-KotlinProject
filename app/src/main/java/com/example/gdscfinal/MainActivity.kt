39e3ea006d037f55258e0e21580a9c87cf38e265
bry <404262470@mail.fju.edu.tw>
2022-05-21 14:51:57 +0800

    use BaseAdapter & change layoutView

--- app/src/main/java/com/example/gdscfinal/MainActivity.kt
+++ app/src/main/java/com/example/gdscfinal/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.gdscfinal
 
-import android.graphics.ImageFormat
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.widget.*
-import org.jsoup.Jsoup
-import android.content.Intent
-import android.net.Uri
 import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import org.jsoup.Jsoup
 
+
+data class News (var title: String = "", var url: String = "", var image: String = ""){
+}
+
+class NewsAdapter(context : Context, resourceId: Int, objects: Array<News>): BaseAdapter(){
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(p0: Int): Any {
+        return ""
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        Log.i("debugView", "in getView")
+        val curNews: News = convertView?.getTag(position) as News
+        val newsImage = convertView?.findViewById(R.id.newsImage) as ImageView
+        val newsText = convertView?.findViewById(R.id.newsText) as TextView
+//        when(position) {
+//            0 -> Picasso.get().load(curNews.url).into(newsImage)
+//
+//            1 -> newsText.text = curNews.title
+//        }
+        Picasso.get().load(curNews.url).into(newsImage);
+        newsText.text = curNews.title
+        Log.i("debugView", curNews.title)
+//        Picasso.with(this@MainActivity).load(currentItem.getImageURL()).into(newsImage)
+        return convertView
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,29 +55,27 @@ class MainActivity : AppCompatActivity() {
             val span = doc.select("h2 > a")
             val img = doc.select("picture > img")
             Log.i("----------", "---------")
-            val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+            val arrayAdapter = ArrayAdapter<News>(this, R.layout.news_view, )
+            var curNews = News()
             for (x in span) {
-                if(x.attr("title").toString()!=""){
-                    Log.i("result",x.attr("title").toString())
-                    arrayAdapter.add(x.attr("title").toString())
-                }
+                if(x.attr("title").toString()!="")
+                    curNews.title = x.attr("title").toString()
+                if(x.attr("data-src").toString()!="")
+                    curNews.image =  x.attr("data-src").toString()
+                if(x.attr("href").toString()!="")
+                    curNews.url = ("https://udn.com/"+x.attr("href").toString())
+                arrayAdapter.add(curNews)
             }
-            for (x in img) {
-                if(x.attr("data-src").toString()!=""){
-                Log.i("img", x.attr("data-src").toString())
-                }
-            }
-            for (x in span) {
-                if(x.attr("href").toString()!=""){
-                    Log.i("link","https://udn.com/"+x.attr("href").toString())
-                }
-            }
+//            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+//            startActivity(browserIntent)
+            Log.i("debug", 1.toString())
             runOnUiThread{
                 list.adapter=arrayAdapter
                 /*list.setOnItemClickListener(
 
                 )*/
             }
+            Log.i("debug", "afterRunOnUiThread")
             /*fun getUrlFromIntent(view: View) {
                 val url = "http://www.google.com"
                 val intent = Intent(Intent.ACTION_VIEW)
@@ -52,9 +83,8 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }*/
         }).start()
-
-        }
     }
+}
 
 
 //https://udn.com/
