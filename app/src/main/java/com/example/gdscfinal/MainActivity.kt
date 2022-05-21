244760ae98a51aa2a08d8dd04a33070e6b2f81d0
TaYu <erici7013787@gmail.com>
2022-05-22 01:26:58 +0800

    完成: 新聞 listView 可點擊、可顯示圖片 尚未完成排版 點擊事件(跳出新聞全文連結)

--- app/src/main/java/com/example/gdscfinal/MainActivity.kt
+++ app/src/main/java/com/example/gdscfinal/MainActivity.kt
@@ -1,78 +1,66 @@
 package com.example.gdscfinal
 
+import android.content.ClipData
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import org.jsoup.Jsoup
+import android.widget.ArrayAdapter
 
 
-data class News (var title: String = "", var url: String = "", var image: String = ""){
-}
 
-class NewsAdapter(context : Context, resourceId: Int, objects: Array<News>): BaseAdapter(){
-    override fun getCount(): Int {
-        return 2
-    }
 
-    override fun getItem(p0: Int): Any {
-        return ""
-    }
-
-    override fun getItemId(p0: Int): Long {
-        return 0
-    }
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        Log.i("debugView", "in getView")
-        val curNews: News = convertView?.getTag(position) as News
-        val newsImage = convertView?.findViewById(R.id.newsImage) as ImageView
-        val newsText = convertView?.findViewById(R.id.newsText) as TextView
-//        when(position) {
-//            0 -> Picasso.get().load(curNews.url).into(newsImage)
-//
-//            1 -> newsText.text = curNews.title
-//        }
-        Picasso.get().load(curNews.url).into(newsImage);
-        newsText.text = curNews.title
-        Log.i("debugView", curNews.title)
-//        Picasso.with(this@MainActivity).load(currentItem.getImageURL()).into(newsImage)
-        return convertView
-    }
-}
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Thread(Runnable{
-            val list=findViewById<ListView>(R.id.listview)
+            val list = findViewById<ListView>(R.id.listview)
+            var newsList = mutableListOf<News>()
+//            val arrayAdapter = ArrayAdapter<String>(this, R.layout.news_view)
             val doc = Jsoup.connect("https://udn.com/news/breaknews/1").get()
-            val span = doc.select("h2 > a")
-            val img = doc.select("picture > img")
+//            Log.i("showJsoup", doc.toString())
+
+            val span = doc.select("div.story-list__image > a")
+//            Log.i("showImg", img.toString())
             Log.i("----------", "---------")
-            val arrayAdapter = ArrayAdapter<News>(this, R.layout.news_view, )
-            var curNews = News()
+            var step = 0
             for (x in span) {
-                if(x.attr("title").toString()!="")
-                    curNews.title = x.attr("title").toString()
-                if(x.attr("data-src").toString()!="")
-                    curNews.image =  x.attr("data-src").toString()
-                if(x.attr("href").toString()!="")
-                    curNews.url = ("https://udn.com/"+x.attr("href").toString())
-                arrayAdapter.add(curNews)
+                val title = x.attr("aria-label")
+                val imageUrl = x.select("picture > *").attr("data-srcset")
+                val url = "https://udn.com/"+x.attr("href")
+                if(title  == "" || imageUrl == "" || url == "")
+                    continue
+                newsList.add(News(x.attr("aria-label"),"https://udn.com/"+x.attr("href"), x.select("picture > *").attr("data-srcset")))
+                Log.i("showImg", imageUrl)
+                Log.i("showTitle", title)
+                Log.i("showUrl", url)
+//                Log.i("showSpan", newsList[step].title)
+//                Log.i("showUrl", newsList[step].url)
+                step += 1
+            }
+            list.setOnItemClickListener { parent:AdapterView<*>, view:View, position:Int, id:Long->
+                when(position){
+                    0 -> Toast.makeText(this, "you click 0", Toast.LENGTH_SHORT).show()
+                    1 -> Toast.makeText(this, "you click 1", Toast.LENGTH_SHORT).show()
+                    2 -> Toast.makeText(this, "you click 2", Toast.LENGTH_SHORT).show()
+                }
             }
 //            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
 //            startActivity(browserIntent)
             Log.i("debug", 1.toString())
             runOnUiThread{
-                list.adapter=arrayAdapter
+                list.adapter = NewsAdapter(this, R.layout.news_view, newsList)
                 /*list.setOnItemClickListener(
-
                 )*/
             }
             Log.i("debug", "afterRunOnUiThread")
